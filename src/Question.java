6a3097903dfff3ad2860a5dfc0afb2fd1dbd1853
Xenbupa <98390430+Xenbupa@users.noreply.github.com>
2022-02-15 10:23:26 +0100

    clase question creada.

--- src/Question.java
+++ src/Question.java
@@ -1,6 +1,27 @@
 public class Question {
 
-    private boolean question;
+    private boolean isCorrect;
+    private String question;
 
+    public Question(boolean isCorrect, String question) {
+        this.isCorrect = isCorrect;
+        this.question = question;
+    }
+
+    public boolean isCorrect() {
+        return isCorrect;
+    }
+
+    public void setCorrect(boolean correct) {
+        isCorrect = correct;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
 
 }
