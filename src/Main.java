59f783f376e9e40aac7b2e42fcd587f269c7626b
EstefaniaRomero <estefaniaromero.torrents@gmail.com>
2022-02-15 10:40:53 +0100

    Creación Arraylist

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
+
+        List<Question> questions = createListQuestion();
+
+    }
+
+    private static List<Question> createListQuestion() {
+        List<Question> questions = new ArrayList<>();
+
+        Question question1 = new Question("Cual es la capital de Portugal?:","Lisboa");
+        Question question2 = new Question("Cuantas caras tiene un dado?:","Seis caras");
+        Question question3 = new Question("De que color es el caballo blanco de SAntiago?:","Blanco");
+        Question question4 = new Question("Quien escribió 100 años de soledad?:","Garcia Marquez");
+        Question question5 = new Question("Quien dirigió el film Indiana Jones?:","Steven Spilberg");
+
+
+        questions.add(question1);
+        questions.add(question2);
+        questions.add(question3);
+        questions.add(question4);
+        questions.add(question5);
+
+        return questions;
     }
 }
