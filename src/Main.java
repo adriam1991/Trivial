f6fe09d38a692b869f620d793c027d46dc2414d0
GerardM5 <78301921+GerardM5@users.noreply.github.com>
2022-02-16 10:30:42 +0100

    correcciones

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,21 @@
 import java.util.*;
 
 public class Main {
+
+    public static final String CULTURILLA_GENERAL = "Culturilla general";
+
     public static void main(String[] args) {
 
 
-        List<Question> questions = createListQuestion();
-        HashMap<String,List<Question>> mapThemes = createMapThemes();
+        HashMap<String, List<Question>> mapThemes = createMapThemes();
 
 
-
-
-        for (Question currentQuestion:questions) {
-            String theme = askForTheme(questions);
-            List<Question> questionsOfTheme = selectTheme(theme,questions);
+        for (Question currentQuestion : questions) {
+            String theme = askForTheme();
+            List<Question> questionsOfTheme = selectTheme(theme, questions);
             printQuestion(currentQuestion);
             String answer = giveAnswer();
-            currentQuestion.setIfIsCorrect(compareResult(answer,currentQuestion));
+            currentQuestion.setIfIsCorrect(compareResult(answer, currentQuestion));
             printResult(currentQuestion);
 
 
@@ -29,12 +29,23 @@ public class Main {
 
     }
 
+    private static String askForTheme() {
+        System.out.println("Que tema quieres jugar?");
+        Scanner sc = new Scanner(System.in);
+        return sc.next();
+    }
+
     private static HashMap<String, List<Question>> createMapThemes() {
-        HashMap<String,List<Question>> mapThemes = new HashMap<>();
+        HashMap<String, List<Question>> mapThemes = new HashMap<>();
 
-        List<Question> artQuestions = createListQuestion("Geografía");
+        List<Question> geoQuestions = createListQuestion("Geografía");
 
-        mapThemes.put("Art",artQuestions);
+        mapThemes.put("Geografía", geoQuestions);
+
+        List<Question> culturaQuestions = createListQuestion(CULTURILLA_GENERAL);
+
+        mapThemes.put(CULTURILLA_GENERAL, geoQuestions);
+
 
 
 
@@ -53,9 +64,9 @@ public class Main {
     }
 
     private static void printResult(Question currentQuestion) {
-        if (currentQuestion.getIfIsCorrect()){
+        if (currentQuestion.getIfIsCorrect()) {
             System.out.println("Correcto");
-        }else System.out.println("A ver si estudiamos mas...");
+        } else System.out.println("A ver si estudiamos mas...");
     }
 
     private static String giveAnswer() {
@@ -72,31 +83,30 @@ public class Main {
         List<Question> questions = new ArrayList<>();
 
         switch (theme) {
-            case "Geografía" :
-                questions.add(new Question("Donde nace el rio Ebro?:","Fontibre","Geografia"));
-                questions.add(new Question("Que rio pasa por París?:","Sena","Geografia"));
-                questions.add(new Question("Cual es la capital de Japón?:","Tokio","Geografia"));
-                questions.add(new Question("Que rio pasa por Londres?", "Tamesis","Geografia"));
-                questions.add(new Question("Cual es la capital de Portugal?:","Lisboa","Geografia"));
+            case "Geografía":
+                questions.add(new Question("Donde nace el rio Ebro?:", "Fontibre", "Geografia"));
+                questions.add(new Question("Que rio pasa por París?:", "Sena", "Geografia"));
+                questions.add(new Question("Cual es la capital de Japón?:", "Tokio", "Geografia"));
+                questions.add(new Question("Que rio pasa por Londres?", "Tamesis", "Geografia"));
+                questions.add(new Question("Cual es la capital de Portugal?:", "Lisboa", "Geografia"));
                 break;
-            case "Culturilla General" :
-                questions.add( new Question("De que color es el caballo blanco de SAntiago?:","Blanco", "Culturilla General"));
-                questions.add(new Question("¿Quién escribió La Odisea?","Homero", "Culturilla General"));
-                questions.add(new Question("¿Qué tipo de animal es la ballena?","Mamifero", "Culturilla General"));
-                questions.add(new Question("¿Qué cantidad de huesos en el cuerpo humano adulto?","206", "Culturilla General"));
-                questions.add(new Question("¿Cuándo acabó la II Guerra Mundial?","1945", "Culturilla General"));
+            case "Culturilla General":
+                questions.add(new Question("De que color es el caballo blanco de SAntiago?:", "Blanco", "Culturilla General"));
+                questions.add(new Question("¿Quién escribió La Odisea?", "Homero", "Culturilla General"));
+                questions.add(new Question("¿Qué tipo de animal es la ballena?", "Mamifero", "Culturilla General"));
+                questions.add(new Question("¿Qué cantidad de huesos en el cuerpo humano adulto?", "206", "Culturilla General"));
+                questions.add(new Question("¿Cuándo acabó la II Guerra Mundial?", "1945", "Culturilla General"));
                 break;
-            case "Diversion" :
-                questions.add(new Question("Cuantas caras tiene un dado?:","Seis caras", "Diversion"));
-                questions.add(new Question("Cuantas caras tiene el cubo de rubick?:","6","Diversion"));
-                questions.add(new Question("¿Qué sube, pero nunca baja?:","Edad","Diversion"));
-                questions.add(new Question("¿Qué entra duro pero sale blando y suave?:","Chicle","Diversion"));
+            case "Diversion":
+                questions.add(new Question("Cuantas caras tiene un dado?:", "Seis caras", "Diversion"));
+                questions.add(new Question("Cuantas caras tiene el cubo de rubick?:", "6", "Diversion"));
+                questions.add(new Question("¿Qué sube, pero nunca baja?:", "Edad", "Diversion"));
+                questions.add(new Question("¿Qué entra duro pero sale blando y suave?:", "Chicle", "Diversion"));
                 break;
             case "Literatura y cine":
-                questions.add(new Question("Quien escribió 100 años de soledad?:","Garcia Marquez","Literatura y cine"));
-                questions.add(new Question("Quien dirigió el film Indiana Jones?:","Steven Spilberg","Literatura y cine"));
+                questions.add(new Question("Quien escribió 100 años de soledad?:", "Garcia Marquez", "Literatura y cine"));
+                questions.add(new Question("Quien dirigió el film Indiana Jones?:", "Steven Spilberg", "Literatura y cine"));
                 break;
-
 
 
         }
